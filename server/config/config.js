af00ae747027c9b57c187d823a13150646014ad8
Riccardo Reali <rreali@my.centennialcollege.ca>
2023-07-12 14:52:56 -0400

    user schema and mongo connection

---
+++ server/config/config.js
@@ -0,0 +1,2 @@
+const mongoURL = "mongodb+srv://rreali1991:rick123@rrsystems.9krxkco.mongodb.net/overflowUser?retryWrites=true&w=majority"
+
--- server/server.js
+++ server/server.js
@@ -1,21 +1,60 @@
-// Creating the server:
-
-// 1)Import express and cors
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
+const User = require('./model/user.model.js');
 
-//2. Create an Express application
 const app = express();
-
 app.use(cors());
 app.use(express.json());
 
-//3 . Create a GET route
-app.get('/', (req, res) => {
-    res.send( console.log("Hello, we are connected!") );
+// Connect to MongoDB
+mongoose.connect('mongodb+srv://rreali1991:rick123@rrsystems.9krxkco.mongodb.net/overflowUser?retryWrites=true&w=majority')
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((error) => {
+    console.error('Error connecting to MongoDB:', error);
+  });
+
+// Register route
+app.post('/api/register', async (req, res) => {
+  try {
+    const user = await User.create({
+      userName: req.body.userName,
+      email: req.body.email,
+      password: req.body.password,
+    });
+    res.json({ status: 'ok' });
+  } catch (err) {
+    res.json({ status: 'error', error: 'duplicate email' });
+  }
 });
 
-//4. Start the server
+// Login route
+app.post('/api/login', async (req, res) => {
+  try {
+    const user = await User.findOne({
+      email: req.body.email,
+      password: req.body.password,
+    });
+
+    if (user) {
+      return res.json({ status: 'ok', user: true });
+    } else {
+      return res.json({ status: 'error', user: false });
+    }
+  } catch (err) {
+    console.error('Error during login:', err);
+    res.json({ status: 'error', error: 'login failed' });
+  }
+});
+
+// Default route
+app.get('/', (req, res) => {
+  res.send('Hello, we are connected!');
+});
+
+// Start the server
 app.listen(8000, () => {
-    console.log(`Server is running on port 8000.`);
-  });
+  console.log(`Server is running on port 8000.`);
+});
