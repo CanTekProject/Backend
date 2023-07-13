ae46aa55dda21d3841ad00df0aa2840e3bf32d58
Riccardo Reali <rreali@my.centennialcollege.ca>
2023-07-12 20:20:37 -0400

    sign up page connected to mongo database, started working on home page css and design similar to figma

--- server/model/user.model.js
+++ server/model/user.model.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-  userName: String,
+  username: String,
   email: String,
   password: String,
 });
 
-const User = mongoose.model('User', userSchema);
+const User = mongoose.models.User || mongoose.model('User', userSchema);
 
 module.exports = User;
--- server/server.js
+++ server/server.js
@@ -1,14 +1,20 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
-const User = require('./model/user.model.js');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(passport.initialize());
 
 // Connect to MongoDB
-mongoose.connect('mongodb+srv://CanTek:CanTek123@cantekcluster.uujud7m.mongodb.net/?retryWrites=true&w=majority')
+mongoose
+  .connect('mongodb+srv://CanTek:CanTek123@cantekcluster.uujud7m.mongodb.net/?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => {
     console.log('Connected to MongoDB');
   })
@@ -16,37 +22,73 @@ mongoose.connect('mongodb+srv://CanTek:CanTek123@cantekcluster.uujud7m.mongodb.n
     console.error('Error connecting to MongoDB:', error);
   });
 
+// User model
+const userSchema = new mongoose.Schema({
+  username: String,
+  email: String,
+  password: String,
+});
+
+const User = mongoose.models.User || mongoose.model('User', userSchema);
+
+// Passport configuration
+passport.use(
+  new LocalStrategy(
+    {
+      usernameField: 'email',
+      passwordField: 'password',
+    },
+    async (email, password, done) => {
+      try {
+        const user = await User.findOne({ email });
+        if (!user) {
+          return done(null, false, { message: 'Incorrect email or password' });
+        }
+        if (user.password !== password) {
+          return done(null, false, { message: 'Incorrect email or password' });
+        }
+        return done(null, user);
+      } catch (error) {
+        return done(error);
+      }
+    }
+  )
+);
+
 // Register route
 app.post('/api/register', async (req, res) => {
   try {
-    const user = await User.create({
-      userName: req.body.userName,
-      email: req.body.email,
-      password: req.body.password,
-    });
-    res.json({ status: 'ok' });
-  } catch (err) {
-    res.json({ status: 'error', error: 'duplicate email' });
+    const { username, email, password } = req.body;
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.json({ status: 'error', message: 'Email already exists' });
+    }
+    const user = await User.create({ username, email, password });
+    return res.json({ status: 'ok', message: 'User registered successfully' });
+  } catch (error) {
+    console.error('Error during registration:', error);
+    return res.json({ status: 'error', message: 'Registration failed' });
   }
 });
 
 // Login route
-app.post('/api/login', async (req, res) => {
-  try {
-    const user = await User.findOne({
-      email: req.body.email,
-      password: req.body.password,
-    });
+app.post('/api/login', (req, res, next) => {
+  passport.authenticate('local', (err, user, info) => {
+    try {
+      if (err || !user) {
+        return res.json({ status: 'error', message: 'Incorrect email or password' });
+      }
 
-    if (user) {
-      return res.json({ status: 'ok', user: true });
-    } else {
-      return res.json({ status: 'error', user: false });
+      req.logIn(user, (err) => {
+        if (err) {
+          return res.json({ status: 'error', message: 'An error occurred during login' });
+        }
+        return res.json({ status: 'ok', message: 'Login successful' });
+      });
+    } catch (error) {
+      return res.json({ status: 'error', message: 'An error occurred during login' });
     }
-  } catch (err) {
-    console.error('Error during login:', err);
-    res.json({ status: 'error', error: 'login failed' });
-  }
+  })(req, res, next);
 });
 
 // Default route
@@ -56,5 +98,5 @@ app.get('/', (req, res) => {
 
 // Start the server
 app.listen(8000, () => {
-  console.log(`Server is running on port 8000.`);
+  console.log('Server is running on port 8000.');
 });
