35190bc8b9b6912d3954fabc9fe150d5733b4772
Riccardo Reali <rreali@my.centennialcollege.ca>
2023-07-16 15:03:03 -0400

    update

--- FrontEnd/src/Pages/Privacy.tsx
+++ FrontEnd/src/Pages/Privacy.tsx
@@ -10,11 +10,9 @@ function Privacy() {
           Privacy Policy outlines how we collect, use, and disclose personal
           information when you use our website or services.
         </p>
-        <p>
-          <hr style={{ margin: "20px", color: "white" }}></hr>
+        <hr style={{ margin: "20px", color: "white" }} />
 
-          <h2>1. Information We Collect</h2>
-        </p>
+        <h2>1. Information We Collect</h2>
         <p>
           We may collect personal information such as your name, email address,
           and contact information when you sign up for an account or interact
@@ -22,52 +20,42 @@ function Privacy() {
           about your usage of our website through cookies and similar
           technologies.
         </p>
-        <p>
-          <hr style={{ margin: "20px", color: "white" }}></hr>
+        <hr style={{ margin: "20px", color: "white" }} />
 
-          <h2>2. How We Use Your Information</h2>
-        </p>
+        <h2>2. How We Use Your Information</h2>
         <p>
           We use the collected information to provide and improve our services,
           personalize your experience, send you relevant communications, and
           ensure the security of our website and users.
         </p>
-        <p>
-          <hr style={{ margin: "20px", color: "white" }}></hr>
+        <hr style={{ margin: "20px", color: "white" }} />
 
-          <h2>3. Information Sharing</h2>
-        </p>
+        <h2>3. Information Sharing</h2>
         <p>
           We may share your personal information with trusted third-party
           service providers who assist us in operating our website and
           delivering our services. We do not sell or rent your personal
           information to third parties for their marketing purposes.
         </p>
-        <p>
-          <hr style={{ margin: "20px", color: "white" }}></hr>
+        <hr style={{ margin: "20px", color: "white" }} />
 
-          <h2>4. Data Retention</h2>
-        </p>
+        <h2>4. Data Retention</h2>
         <p>
           We retain your personal information for as long as necessary to
           fulfill the purposes outlined in this Privacy Policy, unless a longer
           retention period is required or permitted by law.
         </p>
-        <p>
-          <hr style={{ margin: "20px", color: "white" }}></hr>
+        <hr style={{ margin: "20px", color: "white" }} />
 
-          <h2>5. Your Rights</h2>
-        </p>
+        <h2>5. Your Rights</h2>
         <p>
           You have the right to access, correct, update, or delete your personal
           information. Please contact us if you would like to exercise any of
           these rights.
         </p>
-        <p>
-          <hr style={{ margin: "20px", color: "white" }}></hr>
+        <hr style={{ margin: "20px", color: "white" }} />
 
-          <h2>6. Changes to This Privacy Policy</h2>
-        </p>
+        <h2>6. Changes to This Privacy Policy</h2>
         <p>
           We may update this Privacy Policy from time to time. The latest
           version will always be posted on our website, and any significant
@@ -81,4 +69,5 @@ function Privacy() {
     </>
   );
 }
+
 export default Privacy;
